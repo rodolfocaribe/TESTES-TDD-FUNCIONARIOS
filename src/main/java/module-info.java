cc40bd9de6736b359cacfe9cf443d80e48c880e3
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-04-17 08:31:05 -0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module br.edu.atividade {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens br.edu.atividade to javafx.fxml;
+    exports br.edu.atividade;
+}
